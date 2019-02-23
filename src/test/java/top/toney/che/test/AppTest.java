5f7e42341f044ce9ebe05ef521ede7ee1c4650ba
Toney <sunlatus@163.com>
2019-02-23 14:51:34 +0800

    初始化

---
+++ src/test/java/top/toney/che/test/AppTest.java
@@ -0,0 +1,20 @@
+package top.toney.che.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
